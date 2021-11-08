c6403d147860818e1fbc41b5db569b3c7ee03551
MiguelMarcelino <miguelmarcelino@tecnico.ulisboa.pt>
2021-11-08 20:45:44 +0000

    Testing bot

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+import { runInThisContext } from "vm";
+
+require('dotenv').config(); // for .env files
 export default class BotConfiguration {
   private static readonly DISCORD_TOKEN_KEY = 'DISCORD_TOKEN'
   discordToken: string
@@ -5,9 +8,13 @@ export default class BotConfiguration {
   private static readonly MODULES_PATH_KEY = 'MODULES_PATH'
   modulesPath: string
 
+  private static readonly PREFIX_KEY = 'PREFIX'
+  prefix: string
+
   private constructor () {
     this.discordToken = ''
     this.modulesPath = ''
+    this.prefix = ''
   }
 
   static fromEnv (): BotConfiguration {
@@ -18,6 +25,12 @@ export default class BotConfiguration {
       throw new Error(`Missing Discord Token, please set the ${this.DISCORD_TOKEN_KEY} environment variable`)
     }
     config.discordToken = token
+
+    const prefix = process.env[this.PREFIX_KEY]
+    if (prefix === undefined) {
+      throw new Error(`Missing Prefix, please set the ${this.PREFIX_KEY} environment variable`)
+    }
+    config.prefix = prefix
 
     const modulesPath = process.env[this.MODULES_PATH_KEY]
     if (modulesPath === undefined) {
