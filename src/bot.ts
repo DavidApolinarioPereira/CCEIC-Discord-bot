cb17c6e711bab3dd710220e6303f4ce63bedaf99
MiguelMarcelino <miguelmarcelino@tecnico.ulisboa.pt>
2021-11-09 13:01:37 +0000

    Trying to register slash commands

--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,8 @@ import BotConfiguration from './config'
 import { ModuleExecution, ModuleRegistry } from './modules'
 import { ModuleExecutionEnd, ModuleExecutionError, ModuleExecutionEvaluation, ModuleExecutionEvaluationPre, ModuleExecutionFormative, ModuleExecutionFormativeFeedback, ModuleExecutionStart } from './modules/execution'
 import { ExecutionVisitor } from './modules/executionvisitor'
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
 export default class Bot {
   private readonly client: Discord.Client
@@ -12,8 +14,10 @@ export default class Bot {
   constructor (config: BotConfiguration) {
     this.client = new Discord.Client({
       intents: [
-        Discord.Intents.FLAGS.GUILDS
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES
       ]
+  
     })
 
     this.client.on('ready', this.readyHandler.bind(this))
@@ -22,7 +26,7 @@ export default class Bot {
       this.interactionCreateHandler.bind(this)
     })
 
-    this.client.on('message', (message: any) => {
+    this.client.on('messageCreate', (message: any) => {
       console.log(message)
       // if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
@@ -49,6 +53,31 @@ export default class Bot {
 
   readyHandler (): void {
     console.log('Bot up!')
+    this.registerCommands()
+  }
+
+  registerCommands() {
+    const clientId = "907322243586089034"
+    const guildId = "812047505428578364"
+    const commands: Array<string> = []
+    const jsonData = JSON.parse('{"name": "test"}')
+    commands.push(jsonData)
+    const rest = new REST({ version: '9' }).setToken(this.config.discordToken);
+
+    (async () => {
+      try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+          Routes.applicationGuildCommands(clientId, guildId),
+          { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+      } catch (error) {
+        console.error(error);
+      }
+    })();
   }
 
   interactionCreateHandler (interaction: Discord.Interaction): void {
